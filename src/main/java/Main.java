73595c1f8b367fbbf2a01645fa027194d706e73f
oleg kundakov <oleg.kundakov.jazzteam@gmail.com>
2015-11-16 11:09:04 +0300

    fibbonaci example

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import pack.Fib;
+
+/**
+ * Created by JazzTeamUser on 13.11.2015.
+ */
+public class Main {
+    public static void main(String[] t) {
+
+        KieServices ks = KieServices.Factory.get();
+
+        KieContainer kc = ks.getKieClasspathContainer();
+
+        KieSession ksession = kc.newKieSession("ksession1");
+
+        ksession.insert(new Fib(50));
+
+        ksession.fireAllRules();
+    }
+}
