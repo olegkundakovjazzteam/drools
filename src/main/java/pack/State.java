1ea704d8e373869efeaa22ad61712b56c4c8e55c
oleg kundakov <oleg.kundakov.jazzteam@gmail.com>
2015-11-17 17:22:21 +0300

    state example first commit

---
+++ src/main/java/pack/State.java
@@ -0,0 +1,41 @@
+package pack;
+
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ */
+public class State {
+    public static final int NOTRUN = 0;
+    public static final int FINISHED = 1;
+
+    private final PropertyChangeSupport changes =
+            new PropertyChangeSupport(this);
+
+    private String name;
+    private int state;
+
+    public State(String name, int state) {
+        this.name = name;
+        this.state = state;
+    }
+
+    public State() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
