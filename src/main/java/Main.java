043928232cae6521f5b29a8a9299edd5b3341d1e
oleg kundakov <oleg.kundakov.jazzteam@gmail.com>
2015-11-18 11:01:16 +0300

    bank rule works. added new examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,50 @@
+import pack.Cashflow;
 import pack.RuleRunner;
+import pack.TypedCashflow;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  */
 public class Main {
-    //    public static void main(String[] t) {
-//
-//        KieServices ks = KieServices.Factory.get();
-//
-//        KieContainer kc = ks.getKieClasspathContainer();
-//
-//        KieSession ksession = kc.newKieSession("ksession1");
-//
-//
-//        ksession.fireAllRules();
-//    }
+
+
     public static void main(String[] args) throws Exception {
+
+        Object[] cashflows = {
+                new Cashflow(new SimpleDate("01/01/2007"), 300.00),
+                new Cashflow(new SimpleDate("05/01/2007"), 100.00),
+                new Cashflow(new SimpleDate("11/01/2007"), 500.00),
+                new Cashflow(new SimpleDate("07/01/2007"), 800.00),
+                new Cashflow(new SimpleDate("02/01/2007"), 400.00),
+        };
+
+        Object[] cashflows2 = {
+                new TypedCashflow(new SimpleDate("01/01/2007"),
+                        TypedCashflow.CREDIT, 300.00),
+                new TypedCashflow(new SimpleDate("05/01/2007"),
+                        TypedCashflow.CREDIT, 100.00),
+                new TypedCashflow(new SimpleDate("11/01/2007"),
+                        TypedCashflow.CREDIT, 500.00),
+                new TypedCashflow(new SimpleDate("07/01/2007"),
+                        TypedCashflow.DEBIT, 800.00),
+                new TypedCashflow(new SimpleDate("02/01/2007"),
+                        TypedCashflow.DEBIT, 400.00),
+        };
+
+
+//        Number[] nums = {3, 1, 4, 1, 5};
+
         new RuleRunner().runRules(new String[]{"pack/rule.drl"},
-                new Object[0]);
+                cashflows2);
+    }
+}
+
+class SimpleDate extends Date {
+    private static final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+    public SimpleDate(String datestr) throws Exception {
+        setTime(format.parse(datestr).getTime());
     }
 }
---
+++ src/main/java/pack/TypedCashflow.java
@@ -0,0 +1,35 @@
+package pack;
+
+import java.util.Date;
+
+/**
+ * Created by JazzTeamUser on 18.11.2015.
+ */
+public class TypedCashflow extends Cashflow {
+    public static final int CREDIT = 0;
+    public static final int DEBIT = 1;
+
+    private int type;
+
+    public TypedCashflow() {
+    }
+
+    public TypedCashflow(Date date, int type, double amount) {
+        super(date, amount);
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String toString() {
+        return "TypedCashflow[date=" + getDate() +
+                ",type=" + (type == CREDIT ? "Credit" : "Debit") +
+                ",amount=" + getAmount() + "]";
+    }
+}
